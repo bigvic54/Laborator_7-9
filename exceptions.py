aaa2ed4687e7f3826044defe2cbc66cc6d37039f
bigvic54 <74603829+bigvic54@users.noreply.github.com>
2021-02-11 17:07:47 +0200

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,74 @@
+class IDError(Exception):
+    pass
+
+
+class NumeError(Exception):
+    pass
+
+
+class GrupaError(Exception):
+    pass
+
+
+class NrLabError(Exception):
+    pass
+
+
+class NrProbError(Exception):
+    pass
+
+
+class ValidationError(Exception):
+    pass
+
+
+class ParamError(Exception):
+    pass
+
+
+class DescrError(Exception):
+    pass
+
+
+class DeadlineError(Exception):
+    pass
+
+
+class AnError(Exception):
+    pass
+
+
+class LunaError(Exception):
+    pass
+
+
+class ZiError(Exception):
+    pass
+
+
+class IDUnicError(Exception):
+    pass
+
+
+class NrProbUnicError(Exception):
+    pass
+
+
+class ExistentaError(Exception):
+    pass
+
+
+class UnicitateAsignareError(Exception):
+    pass
+
+
+class NotareError(Exception):
+    pass
+
+
+class NotaError(Exception):
+    pass
+
+
+class RevError(Exception):
+    pass
